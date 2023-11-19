50701c8edd0578caa8cce32753b47b3307dd6812
BrandingTech <147016857+BrandingTech@users.noreply.github.com>
2023-11-19 14:14:41 +0000

    Added dynamic data and add api route

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+DATA = [
+  {
+    'id':1 ,
+    'title':'data 1',
+    'point':'pint 1',
+    'color': 'red'
+  },
+  {
+    'id':2 ,
+    'title':'data 2',
+    'point':'pint 2',
+    'color': 'green'
+  },
+  {
+    'id':3 ,
+    'title':'data 3',
+    'point':'pint 3',
+    'color': 'red 3'
+  },
+  {
+    'id':4 ,
+    'title':'data 4',
+    'point':'pint 4',
+    'color': 'green 4'
+  },
+]
 
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def hello_brandingtech():
+  return render_template('home.html',
+                         data=DATA)
+
+@app.route("/api/data")
+def list_data():
+  return jsonify(DATA)
 
 
 if __name__ == "__main__":
